d5d0fce644d4af6817b24baa92d5a1e7bccab2eb
akrava <arkadiy@ex.ua>
2021-10-11 15:55:26 +0300

    fix

--- barcode-generator-program/src/preload.ts
+++ barcode-generator-program/src/preload.ts
@@ -2,13 +2,13 @@
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
     const replaceText = (selector: string, text: string) => {
-      const element = document.getElementById(selector);
-      if (element) {
-        element.innerText = text;
-      }
+        const element = document.getElementById(selector);
+        if (element) {
+            element.innerText = text;
+        }
     };
-  
+
     for (const type of ["chrome", "node", "electron"]) {
-      replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+        replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
     }
-  });
+});
